eca37389ff4f7fd22f5e87c640261c5c32ad29e2
Clicker00 <suwinsky@wp.pl>
2023-02-15 21:03:11 +0100

    Add some comments and changed rendering to redirecting where needed

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -13,7 +13,7 @@ def entry(request, title):
         return render(request, "encyclopedia/error.html", {
             "error": 404
         })
-    print([x.lower() for x in util.list_entries()])
+    # Finding an index of the title in order to prompt it on the page case insensitively
     index = [x.lower() for x in util.list_entries()].index(title.lower())
     return render(request, "encyclopedia/entry.html", {
         "title": util.list_entries()[index],
@@ -31,17 +31,14 @@ def search(request):
             # Finding the index of the entry in list_entries
             # This allows to refer to the original title without many operations on "q" query from the HTML form
             index = [x.lower() for x in util.list_entries()].index(q.lower())
-            
-            return render(request, "encyclopedia/entry.html", {
-                "title": util.list_entries()[index],
-                "content": util.get_entry(q)
-            })
+            return redirect("encyclopedia:entry", title=util.list_entries()[index])
+        # If the user has not provided the full title, then look if the query is a substring
+        # in any of existing pages' titles 
         else:
             result_list = []
             for i in range(len(util.list_entries())):
                 if q.lower() in util.list_entries()[i].lower():
                     result_list.append(util.list_entries()[i])    
-            print(result_list)
             return render(request, "encyclopedia/search.html", {
                     "query": q,
                     "result_list": result_list
@@ -50,6 +47,8 @@ def search(request):
 def newpage(request):
     if request.method == "POST":
         title = request.POST.get("title")
+        # Additional checks in case user bypassed frontend requirements
+        # or if article with given title already exists
         if not title:
             return render(request, "encyclopedia/error.html", {
                 "error": "No title provided"
@@ -65,16 +64,16 @@ def newpage(request):
                 "error": "Please provide the content"
             })
         util.save_entry(title, newcontent)
-        return render(request, "encyclopedia/entry.html", {
-            "title": title,
-            "content": newcontent
-        })
+        return redirect("encyclopedia:entry", title=str(title))
     else:
         return render(request, "encyclopedia/newpage.html")
     
 def editpage(request, title):
     
     if request.method == "POST":
+        if request.POST.get("button") == "delete":
+            util.delete_entry(title)
+            return redirect("encyclopedia:index")
         newtitle = request.POST.get("newtitle")
         newcontent = request.POST.get("newcontent")
         if title == newtitle:
@@ -97,6 +96,7 @@ def editpage(request, title):
 
 def randompage(request):
     list_of_titles = util.list_entries()
+    # Getting random index of list of titles
     index = random.randint(0, len(list_of_titles) - 1)
     title = list_of_titles[index]
-    return redirect("wiki/" + title)
+    return redirect("encyclopedia:entry", title=str(title))
